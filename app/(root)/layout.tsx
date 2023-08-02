497445f3681a8769d10269259715a409097b803b
MatthieuTricoire <matthieutricoire@gmail.com>
2023-08-02 15:21:22 +0100

    Search bar finished
    Search bar initialized with a debounce function

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/navbar'
+import Sidebar from '@/components/sidebar'
+import React from 'react'
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <div className='h-full'>
+    <Navbar />
+    <div className='hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0'>
+      <Sidebar />
+    </div>
+    <main className='md:pl-20 pt-16 h-full'>{children}</main>
+  </div>
+)
+
+export default RootLayout
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from '@clerk/nextjs'
+
+import { ThemeProvider } from '@/components/theme-provider'
+
+import { cn } from '@/lib/utils';
+
+import './globals.css'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'IA Companion - From Antonio',
-  description: 'A tutorial realized thanks to Antonio',
+  title: 'Companion.AI',
+  description: 'Your customized companion.',
 }
 
 export default function RootLayout({
@@ -16,10 +22,13 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning >
+        <body className={cn("bg-secondary", inter.className)}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
-
 }
